7ca0eaa0989dda41f53bd1a65d54899003f55e1b
stxss <93729036+stxss@users.noreply.github.com>
2023-01-18 22:58:20 +0000

    Add the board layout and basic game mechanicplay of assigning the characters to positions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -17,7 +17,7 @@ class Game
     @player2 = Player.new(player2_name, player2_char, 0)
 
     # After having the necesseary information for both players, starting a new board
-    @game = Board.new
+    @game = Board.new(player1_char, player2_char)
   end
 
   # Method for asking the name
@@ -54,20 +54,59 @@ end
 
 # Creating the class for the Tic-Tac-Toe board
 class Board
-  def initialize
-    # Setting a flag to know if a winner was found
-    @is_winner = false
+  attr_accessor :board, :is_winner, :turn
 
-    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    puts "\n"
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  def initialize(player1_char, player2_char)
+    # Setting a flag to know if a winner was found
+    is_winner = false
+
+    # Creating the array for the rows and columns of the board
+    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    # Playing a round, passing the players' characters and whose turn is it to play
+    @turn = 1
+    until is_winner
+      round(player1_char, player2_char, turn)
+      @turn += 1
+
+      is_winner = true if turn == 10
+    end
+  end
+
+  def round(player1_char, player2_char, turn)
+    play = if turn.odd?
+             'first'
+           else
+             'second'
+           end
+
+    placing = player_play.to_i - 1
+    row = placing / 3
+    column = placing % 3
+
+    case play
+    when 'first'
+      board[row][column] = player1_char
+    when 'second'
+      board[row][column] = player2_char
+    end
+    print_board
+  end
+
+  def player_play
+    puts "Please, select the position you want to play in: \n"
+    gets.chomp
+  end
+
+  def print_board
+    puts "#{board[0][0]} | #{board[0][1]} | #{board[0][2]}\n"
     puts '---+---+---'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "#{board[1][0]} | #{board[1][1]} | #{board[1][2]} \n"
     puts '---+---+---'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts "#{board[2][0]} | #{board[2][1]} | #{board[2][2]} \n"
   end
 end
 
 start = Game.new
-p start.game.plays
+p start.game.round
 # puts start
