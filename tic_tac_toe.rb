864922c6efb6b7b2affe52e5ae8a5e4f62596f72
stxss <93729036+stxss@users.noreply.github.com>
2023-01-19 18:36:44 +0000

    Add verification for the characters to be a letter, from a-Z (can be either upper or lower case), for them to be of length of '1' and for them not to be equal between each player

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,16 +7,24 @@ class Game
   # Upon initialization, asking for the players names and playing characters
   def initialize
     @player1_name = ask_name('first')
-    @player1_char = ask_char('first')
+
+    loop do
+      @player1_char = ask_char('first')
+      break if player1_char.length == 1 && player1_char.match(/[a-zA-Z]/)
+    end
+
     @player1 = Player.new(player1_name, player1_char, 0)
 
-    # @player2_name = ask_name('second')
     loop do
       @player2_name = ask_name('second')
       break unless player2_name == player1_name
     end
 
-    @player2_char = ask_char('second')
+    loop do
+      @player2_char = ask_char('second')
+      break if player2_char != player1_char && player2_char.match(/[a-zA-Z]/) && player2_char.length == 1
+    end
+
     @player2 = Player.new(player2_name, player2_char, 0)
 
     # After having the necesseary information for both players, starting a new board
@@ -27,9 +35,9 @@ class Game
   def ask_name(order)
     case order
     when 'first'
-      puts "Please, enter the name of the first player: \n\n"
+      puts "Please, enter the name of the first player: \n"
     when 'second'
-      puts "\nPlease, enter the name of the second player: \n\n"
+      puts "\nPlease, enter the name of the second player: \n"
     end
     gets.chomp
   end
@@ -38,9 +46,10 @@ class Game
   def ask_char(order)
     case order
     when 'first'
-      puts "\n#{@player1_name}, please enter a character you want to play with, from 'a-z'\n\n"
+      puts "\n#{player1_name}, please enter a character you want to play with, from 'a-Z'. Use only 1 character.\n"
     when 'second'
-      puts "\n#{@player2_name}, please enter a character you want to play with, from 'a-z'\n\n"
+      puts "\n#{player2_name}, please enter a character you want to play with, from 'a-Z'. Use only 1 character.\n"
+      puts "It can't be #{player1_char}!\n\n"
     end
     gets.chomp
   end
@@ -104,7 +113,7 @@ class Board
 
   # Function to get the player's play
   def player_play
-    puts "Please, select the position you want to play in: \n"
+    puts "\nPlease, select the position you want to play in: \n"
     gets.chomp
   end
 
