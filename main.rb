6b58437f5308fe8be1be63ebf75ce03b02fe610f
stxss <93729036+stxss@users.noreply.github.com>
2023-01-20 17:16:15 +0000

    Add comment and refactor the code a bit

--- main.rb
+++ main.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# Creating the class to store the players info
+class Player
+  attr_reader :name, :character
+  attr_accessor :score
+
+  def initialize(name, character, score)
+    @name = name
+    @character = character
+    @score = score
+  end
+end
+
 # Creating the Game Class, which stores the player's names, selected character and score
 class Game
   # Names and selected characters only need to be read, while the game/score may need to be changed
@@ -8,15 +20,17 @@ class Game
 
   # Upon initialization, asking for the players names and playing characters
   def initialize(p1, p2)
+    # If the players are new, aka if it's a completely new game, create new profiles
     if p1 == 'new' || p2 == 'new'
-
       @player1_name = ask_name('first')
 
+      # Loop asking for the player's character until it satisfies the conditions of having the length of 1 and being a letter, case doesn't matter
       loop do
         @player1_char = ask_char('first')
         break if player1_char.length == 1 && player1_char.match(/[a-zA-Z]/)
       end
 
+      # Loop asking the name of the second player if it is equal to the name of the first
       loop do
         @player2_name = ask_name('second')
         break unless player2_name == player1_name
@@ -27,6 +41,7 @@ class Game
         break if player2_char != player1_char && player2_char.match(/[a-zA-Z]/) && player2_char.length == 1
       end
 
+      # Setting base score to 0
       @player1_score = 0
       @player2_score = 0
 
@@ -35,9 +50,9 @@ class Game
       @player2 = Player.new(player2_name, player2_char, player2_score)
 
       # After having the necesseary information for both players, starting a new board
-      # @game = Board.new(player1_char, player2_char)
       @game = Board.new(@player1, @player2)
     else
+      # If the players have already played some rounds, just create a new board, no need to 'register' new players
       @game = Board.new(p1, p2)
     end
   end
@@ -46,7 +61,7 @@ class Game
   def ask_name(order)
     case order
     when 'first'
-      puts "Please, enter the name of the first player: \n"
+      puts "\nPlease, enter the name of the first player: \n"
     when 'second'
       puts "\nPlease, enter the name of the second player: \n"
     end
@@ -60,21 +75,9 @@ class Game
       puts "\n#{player1_name}, please enter a character you want to play with, from 'a-Z'. Use only 1 character.\n"
     when 'second'
       puts "\n#{player2_name}, please enter a character you want to play with, from 'a-Z'. Use only 1 character.\n"
-      puts "It can't be #{player1_char}!\n\n"
+      puts "It can't be '\e[1m#{player1_char}\e[0m'!\n\n"
     end
     gets.chomp
-  end
-end
-
-# Creating the class to store the players info
-class Player
-  attr_reader :name, :character
-  attr_accessor :score
-
-  def initialize(name, character, score)
-    @name = name
-    @character = character
-    @score = score
   end
 end
 
@@ -93,11 +96,12 @@ class Board
     # Creating the array for the rows and columns of the board
     @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-    # Playing a round, passing the players' characters and whose turn is it to play
+    # Playing a round, passing the players' characters and whose turn is it to play, until there is a winner
     @turn = 1
     until @is_winner
       round(turn)
 
+      # If all the squares are occupied and there's no winner, set a tie and prompt for a restart
       if turn == 10 && @is_winner == false
         puts "Okay, no more places are available and there is no winner, so it's a tie!\n"
         restart
@@ -135,11 +139,14 @@ class Board
     # Printing out the board
     print_board
 
+    # Unless there is a winner, continue playing
     return unless win_check(@player1, @player2)
 
+    # Prompt for the restart if there's a winner of the round
     restart
   end
 
+  # Method for restart
   def restart
     loop do
       puts "\nDo you want to play again? Please enter a valid option. [Y/N]"
@@ -153,7 +160,7 @@ class Board
     end
   end
 
-  # Function to print out the board, updating it as well on each round
+  # Function to print out the board, updating it on each played round
   def print_board
     system('clear')
     puts "           #{board[0][0]} | #{board[0][1]} | #{board[0][2]}      | \e[4mPlayer scores\e[0m\n"
@@ -163,35 +170,42 @@ class Board
     puts "           #{board[2][0]} | #{board[2][1]} | #{board[2][2]}      |\n"
   end
 
+  # Method to check for a winner
   def win_check(player1, player2)
+    # Unless there is a win, continue playing
     return unless row_win? || col_win? || diag_win?
 
+    # Seeing what was the winning marker and assigning the winner accordingly
     winner = if marker == player1.character
                player1
              else
                player2
              end
 
+    # Updating the winner's score, as well as the board, and flagging the is_winner to true, to prompt for a new round
     winner.score += 1
     puts "\nThere's a winner! The winner is #{winner.name} and their score is #{winner.score}\n"
     print_board
     @is_winner = true
   end
 
+  # Method for a row win
   def row_win?
     (0..2).any? do |row|
       board[row].all?(marker)
     end
   end
 
+  # Method for a column win
   def col_win?
     (0..2).any? do |col|
       board.transpose[col].all?(marker)
     end
   end
 
+  # Method for a diagonal win
   def diag_win?
-    # Top left to bottom right diagonal
+    # Top left to bottom right diagonal (tlbr) and bottom left to top right diagonal (bltr)
     tlbr_diag = []
     bltr_diag = []
 
