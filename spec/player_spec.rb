e34081aa4346593c6970ab13c098c65ea3d17859
stxss <93729036+stxss@users.noreply.github.com>
2023-03-04 18:42:19 +0000

    Introduce spec for the Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,99 +1,120 @@
 require_relative("../lib/player")
 
 describe Player do
-    describe "#ask_name" do
-        context "asks player for name" do
-            subject(:player) { described_class.new }
+  describe "#ask_name" do
+    context "asks player for name" do
+      subject(:player) { described_class.new }
 
-            before do
-                order = "first"
-                valid_name = "ruby"
-                allow(player).to receive(:ask_name).with(order).and_return(valid_name)
-            end
+      before do
+        order = "first"
+        valid_name = "ruby"
+        allow(player).to receive(:ask_name).with(order).and_return(valid_name)
+      end
 
-            it "stops loop and does not display error message regarding name" do
-                error_message = "You can't use another player's name!"
-                expect(player).not_to receive(:puts).with(error_message)
-                player.ask_name("first")
-            end
-        end
+      it "stops loop and does not display error message regarding name" do
+        error_message = "You can't use another player's name!"
+        expect(player).not_to receive(:puts).with(error_message)
+        player.ask_name("first")
+      end
+    end
+  end
+
+  describe "#ask_char" do
+    context "asks player for character" do
+      subject(:player) { described_class.new }
+
+      before do
+        order = "first"
+        valid_char = "ruby"
+        allow(player).to receive(:ask_char).with(order).and_return(valid_char)
+      end
+
+      it "stops loop and does not display error message regarding character" do
+        error_message = "You can't use another player's character!"
+        expect(player).not_to receive(:puts).with(error_message)
+        player.create_player("first")
+      end
+    end
+  end
+
+  describe "#verify_name" do
+    context "verifies name" do
+      subject(:player) { described_class.new }
+
+      it "returns name if valid" do
+        input = "valid"
+        order = "first"
+        verified = player.verify_name(order, input)
+        expect(verified).to eq("valid")
+      end
     end
 
-    describe "#ask_char" do
-        context "asks player for character" do
-            subject(:player) { described_class.new }
+    context "if a name already exists" do
+      subject(:player) { described_class.new }
 
-            before do
-                order = "first"
-                valid_char = "ruby"
-                allow(player).to receive(:ask_char).with(order).and_return(valid_char)
-            end
-
-            it "stops loop and does not display error message regarding character" do
-                error_message = "You can't use another player's character!"
-                expect(player).not_to receive(:puts).with(error_message)
-                player.create_player("first")
-            end
-        end
+      it "returns name if valid" do
+        input = "ruby"
+        order = "second"
+        player.instance_variable_set(:@player1_name, "valid")
+        verified = player.verify_name(order, input)
+        expect(verified).to eq("ruby")
+      end
     end
 
-    describe "#verify_name" do
-        context "verifies name" do
-            subject(:player) { described_class.new }
+    context "if invalid" do
+      subject(:player) { described_class.new }
 
-            it "returns name if valid" do
-                input = "valid"
-                order = "first"
-                verified = player.verify_name(order, input)
-                expect(verified).to eq("valid")
-            end
-        end
+      it "returns nil" do
+        input = "valid"
+        order = "second"
+        player.instance_variable_set(:@player1_name, "valid")
+        verified = player.verify_name(order, input)
+        expect(verified).to be_nil
+      end
+    end
+  end
 
-        context "verifies name if a name already exists" do
-            subject(:player) { described_class.new }
+  describe "#verify_char" do
+    context "verifies character" do
+      subject(:player) { described_class.new }
 
-            it "returns name if valid" do
-                input = "ruby"
-                order = "second"
-                player.instance_variable_set(:@player1_name, "valid")
-                verified = player.verify_name(order, input)
-                expect(verified).to eq("ruby")
-            end
-        end
-
-        context "verifies name if invalid" do
-            subject(:player) { described_class.new }
-
-            it "returns nil" do
-                input = "valid"
-                order = "second"
-                player.instance_variable_set(:@player1_name, "valid")
-                verified = player.verify_name(order, input)
-                expect(verified).to be_nil
-            end
-        end
+      it "returns character if valid" do
+        input = "x"
+        order = "first"
+        verified = player.verify_name(order, input)
+        expect(verified).to eq("x")
+      end
     end
 
-    describe "#verify_char" do
-        context "verifies character" do
-            subject(:player) { described_class.new }
+    context "if a char already exists" do
+      subject(:player) { described_class.new }
 
-            xit "returns character if valid" do
-            end
-        end
+      it "returns char if valid" do
+        input = "o"
+        order = "second"
+        player.instance_variable_set(:@player1_character, "x")
+        verified = player.verify_name(order, input)
+        expect(verified).to eq("o")
+      end
 
-        context "verifies char if a char already exists" do
-            subject(:player) { described_class.new }
-
-            xit "returns char if valid" do
-            end
-        end
-
-        context "verifies character if invalid" do
-            subject(:player) { described_class.new }
-
-            xit "returns nil" do
-            end
-        end
+      it "returns nil if char is equal to the other player's char" do
+        input = "x"
+        order = "second"
+        player.instance_variable_set(:@player1_character, "x")
+        verified = player.verify_char(order, input)
+        expect(verified).to be_nil
+      end
     end
+
+    context "if invalid" do
+      subject(:player) { described_class.new }
+
+      it "returns nil" do
+        input = "4"
+        order = "first"
+        verified = player.verify_char(order, input)
+        expect(verified).to be_nil
+      end
+    end
+  end
 end
